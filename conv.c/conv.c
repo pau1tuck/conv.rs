eac960cda5744766ec19daefed2808456aedd28c
Paul Tuck <paul.tuck@oxon.org>
2023-07-05 22:52:12 +0700

    "2023-07-05_22:52+07"

--- conv.c/conv.c
+++ conv.c/conv.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double GBP_THB = 44.3490;
+double THB_GBP = 0.0225;
+
 int main()
 {
-    printf("Hello, world!\n");
+    printf("Hello, World!\n");
     return 0;
 }
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+fn main() {
+    //  A hashmap with string slices (&str) as keys and another hashmap as values:
+    let mut conversion_rates: HashMap<&str, HashMap<&str, f64>> = HashMap::new();
+
+    println!("Hello, World!");
+}
