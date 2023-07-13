6723d016870a5a9959602a2f1e727c482c660faf
Paul Tuck <paul.tuck@oxon.org>
2023-07-14 02:03:56 +0700

    "2023-07-14_02:03+07"

--- nodejs/public/currency.js
+++ nodejs/public/currency.js
@@ -4,12 +4,17 @@ const tslib_1 = require("tslib");
 const fs_1 = tslib_1.__importDefault(require("fs"));
 const path_1 = tslib_1.__importDefault(require("path"));
 const fetch_rates_1 = require("./fetch-rates");
-const lastUpdate = null;
-fs_1.default.access(path_1.default.join(process.cwd(), "data/rates.json"), fs_1.default.constants.F_OK, err => {
-    if (err) {
-        (0, fetch_rates_1.fetchRates)();
-    }
-    else {
-        const lastUpdate = JSON.parse(fs_1.default.readFileSync(path_1.default.join(process.cwd(), "data/rates.json"), "utf8"));
-    }
-});
+const ratesPath = path_1.default.join(process.cwd(), "data/rates.json");
+let rates = null;
+let lastUpdate = null;
+const readRates = () => {
+    rates = JSON.parse(fs_1.default.readFileSync(ratesPath, "utf8"));
+};
+if (fs_1.default.existsSync(ratesPath)) {
+    console.log("Currency rate data exists.");
+    readRates();
+}
+else {
+    console.log("Currency rate file does not exist. Fetching rates...");
+    (0, fetch_rates_1.fetchRates)();
+}
--- nodejs/src/currency.ts
+++ nodejs/src/currency.ts
@@ -2,28 +2,27 @@ import fs from "fs";
 import path from "path";
 import { fetchRates } from "./fetch-rates";
 
+// Initialize variables
+const ratesPath = path.join(process.cwd(), "data/rates.json");
+let rates = null;
+let lastUpdate: any = null;
+
+// Check the existence of rates.json or update()
+const readRates = () => {
+    rates = JSON.parse(fs.readFileSync(ratesPath, "utf8"));
+};
+
+if (fs.existsSync(ratesPath)) {
+    console.log("Currency rate data exists.");
+    readRates();
+} else {
+    console.log("Fetching latest currency rates...");
+    fetchRates();
+}
+
 // Convert today's date into YYYY-MM-DD form
 
 // Check the first entry in rates.json and compare it with the current data
-
-const lastUpdate: any = null;
-
-fs.access(
-    path.join(process.cwd(), "data/rates.json"),
-    fs.constants.F_OK,
-    err => {
-        if (err) {
-            fetchRates();
-        } else {
-            const lastUpdate = JSON.parse(
-                fs.readFileSync(
-                    path.join(process.cwd(), "data/rates.json"),
-                    "utf8",
-                ),
-            );
-        }
-    },
-);
 
 // If they're the same then import the data from rates.json
 
