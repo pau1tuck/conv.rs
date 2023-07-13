343e96c55f4cdb1f25e1ad6f2da7e6460026f13c
Paul Tuck <paul.tuck@oxon.org>
2023-07-14 04:43:33 +0700

    "2023-07-14_04:43+07"

--- nodejs/src/fetch-rates.ts
+++ nodejs/src/fetch-rates.ts
@@ -3,11 +3,7 @@ import path from "path";
 import axios from "axios";
 // import config from "./config.json" assert { type: "json" };
 
-/*const settings = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, "../config.json"), "utf-8"),
-);*/
-
-// const RATES_URL = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+// Read all currency codes and names from currencies.json
 // console.time("Done in");
 const currencies = JSON.parse(
     fs.readFileSync(path.join(process.cwd(), "data/currencies.json"), "utf8"),
@@ -16,7 +12,10 @@ const currencies = JSON.parse(
 const rates: any = [];
 
 export const fetchRates = async () => {
+    // Measure the time it takes to retrieve all of the data
     const start = process.hrtime();
+
+    // Map over the currencies object in currencies.json
     const keys = Object.keys(currencies);
     const promises = keys.map(async key => {
         try {
@@ -30,17 +29,18 @@ export const fetchRates = async () => {
             throw new Error(`Error: ${err}`);
         }
     });
-
     await Promise.all(promises);
 
+    // Write the downloaded data to rates.json
     fs.writeFileSync(
         path.join(process.cwd(), "data/rates.json"),
         JSON.stringify(rates, null, 2),
     );
+
+    // Measure and write the data retrieval time to stdout
     const hrtime = process.hrtime(start);
     const elapsed = hrtime[0] + hrtime[1] / 1e9; // convert to seconds
     // const elapsed = hrtime[0] * 1000 + hrtime[1] / 1e6; // convert to milliseconds
-
     process.stdout.write(`Done in ${elapsed.toFixed(3)}ms.\n`);
     // console.timeEnd("Done in");
 };
