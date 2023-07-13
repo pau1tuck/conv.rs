814967077103a636128ab8d8c6b8bc350b9e17cc
Paul Tuck <paul.tuck@oxon.org>
2023-07-14 02:20:05 +0700

    "2023-07-14_02:20+07"

--- nodejs/public/currency.js
+++ nodejs/public/currency.js
@@ -5,10 +5,12 @@ const fs_1 = tslib_1.__importDefault(require("fs"));
 const path_1 = tslib_1.__importDefault(require("path"));
 const fetch_rates_1 = require("./fetch-rates");
 const ratesPath = path_1.default.join(process.cwd(), "data/rates.json");
-let rates = null;
+let rates = [];
 let lastUpdate = null;
 const readRates = () => {
     rates = JSON.parse(fs_1.default.readFileSync(ratesPath, "utf8"));
+    lastUpdate = rates[0].date;
+    console.log(lastUpdate);
 };
 if (fs_1.default.existsSync(ratesPath)) {
     console.log("Currency rate data exists.");
--- nodejs/src/currency.ts
+++ nodejs/src/currency.ts
@@ -4,19 +4,21 @@ import { fetchRates } from "./fetch-rates";
 
 // Initialize variables
 const ratesPath = path.join(process.cwd(), "data/rates.json");
-let rates = null;
+let rates: any = [];
 let lastUpdate: any = null;
 
 // Check the existence of rates.json or update()
 const readRates = () => {
     rates = JSON.parse(fs.readFileSync(ratesPath, "utf8"));
+    lastUpdate = rates[0].date;
+    console.log(lastUpdate);
 };
 
 if (fs.existsSync(ratesPath)) {
     console.log("Currency rate data exists.");
     readRates();
 } else {
-    console.log("Fetching latest currency rates...");
+    console.log("Fetching currency rate data...");
     fetchRates();
 }
 
